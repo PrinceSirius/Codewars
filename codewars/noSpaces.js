349c41fb64779bd57e1fbdba938ad0534fef2c9e
PrinceSirius <alzumar16@yahoo.com>
2022-05-18 20:41:37 -0500

    No Spaces

---
+++ codewars/noSpaces.js
@@ -0,0 +1,7 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(x){
+    return x.split(" ").join("");
+}
+
+//This function takes the array and turns it into a new one without spaces
